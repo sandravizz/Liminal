6a7d65bfe06a3a611a56aa83db30dd5acf3a4153
Sandra Becker <sandra.becker1982@gmail.com>
2025-03-04 22:20:23 +0000

    button styling change layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SankeyChart from './components/sankeyChart/sankeychart';
 import SankeychartEntity from './components/sankeyChart/sankeychartEntity';
 import SankeychartPerson from './components/sankeyChart/sankeychartPerson';
 
-
 function App() {
   return (
     <Router>
@@ -14,6 +13,16 @@ function App() {
           </p>
           <p class="chart_title">
             Who is the biggest investor, which is the biggest investment and how are they related.  
+          </p>
+          <p class="footer">
+            <strong>Interactivity </strong>
+            When hover over either an investments on the left side or an investors on the right sight we can see the details.           
+          </p><p class="footer">  
+            <strong>Animation </strong>
+            Order by investment size in dollar the connections are appearing one by one starting with the biggest.             
+          </p><p class="footer">
+            <strong>How to read the chart </strong>           
+            There are basically tree charts in one. We see the investments in the form of stacked bar chart to the left and the investors to the right. The sankey allows us to see their connections in comparison with all connections hence investments made. 
           </p>
         <nav>
           <ul>
@@ -27,16 +36,7 @@ function App() {
           <Route path="/SankeychartPerson" element={<SankeychartPerson />}></Route>
           <Route path="/SankeychartEntity" element={<SankeychartEntity />}></Route>
         </Routes>
-        <p class="footer">
-            <strong>Interactivity</strong>
-            When hover over either an investments on the left side or an investors on the right sight we can see the details.           
-          </p><p class="footer">  
-            <strong>Animation</strong>
-            Order by investment size in dollar the connections are appearing one by one starting with the biggest.             
-          </p><p class="footer">
-            <strong>How to read the chart</strong>           
-            There are basically tree charts in one. We see the investments in the form of stacked bar chart to the left and the investors to the right. The sankey allows us to see their connections in comparison with all connections hence investments made. 
-          </p>
+
       </div>
  
     </Router>
